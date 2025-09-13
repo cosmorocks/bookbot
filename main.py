28339cc393f21e62a312efce810c5adc638e47ec
Matthew Han <matt3337@gmail.com>
2025-09-13 16:52:38 -0400

    add feature to print sorted character counts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sort_num_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,9 +6,17 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     booktext = get_book_text("./books/frankenstein.txt")
-    print(f"{get_num_words(booktext)} words found in the document")
-    print(get_num_chars(booktext))
+    print(f"Found {get_num_words(booktext)} total words")
+    print("--------- Character Count -------")
+    formatted_num_chars = get_num_chars(booktext)
+    sort_num_chars(formatted_num_chars)
+    for char in formatted_num_chars:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -13,5 +13,16 @@ def get_num_chars(booktext):
     # if already in dict, add 1 to current count
         else: 
             num_chars[character] = num_chars.get(character) + 1
-    # return dict
+    # cleaning up dict (standardizing it)
+    num_chars_formatted = []
+    for char, count in num_chars.items():
+        num_chars_formatted.append({"char":char, "num":count})
+
+    return num_chars_formatted
+
+def sort_on(items):
+    return items["num"]
+
+def sort_num_chars(num_chars):
+    num_chars.sort(reverse=True,key=sort_on)
     return num_chars
